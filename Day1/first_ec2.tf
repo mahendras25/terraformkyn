6414ff1ae89b913429e57c76e23c67e2c4b1c9cd
mahendras25 <mahendras.tulipit@hotmail.com>
2023-11-27 15:26:03 +0530

    commit27nov23

--- Day1/first_ec2.tf
+++ Day1/first_ec2.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA4K6IJLWFGBISVTWF"
-  secret_key = "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"
+  access_key = "AKIA5I2DFJ3YPEJCQCEW"
+  secret_key = "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"
 }
 
-resource "aws_instance" "hwksrmyec2" {
+resource "aws_instance" "mpss_myec2" {
     ami = "ami-00c39f71452c08778"
     instance_type = "t2.micro"
 }
