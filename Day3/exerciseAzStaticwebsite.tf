1bd42dba152a00389cfb330d5fdf2d2614164469
sampletrngithub01 <135112497+sampletrngithub01@users.noreply.github.com>
2023-11-29 16:07:20 +0530

    Update exerciseAzStaticwebsite.tf

--- Day3/exerciseAzStaticwebsite.tf
+++ Day3/exerciseAzStaticwebsite.tf
@@ -1,1 +1,8 @@
+Create a Azure web app static site.
+Follow the documentation(Terraform)
+Use the resourcee provided block which we used in Day 2 - Subscription id and Tenant ID.
+
+
+
+
 https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/static_site
