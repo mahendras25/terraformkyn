8b4151cf4aa28f2006ea5e0005d2a80e82cb5ae5
sampletrngithub01 <135112497+sampletrngithub01@users.noreply.github.com>
2023-12-01 05:26:21 +0530

    Create TFAWSBackend.tf

---
+++ Day5/TFAWSBackend.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = "us-west-2"
+}
+terraform {
+  backend "s3" {
+    bucket = "kplabs-terraform-backend"
+    key    = "network/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_eip" "lb" {
+  vpc      = true
+}
