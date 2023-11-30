f4c65e5439d5b1cd02ac324cdb31fe9f7fe85e14
sampletrngithub01 <135112497+sampletrngithub01@users.noreply.github.com>
2023-11-30 13:10:26 +0530

    Update localexec.tf

--- Day4/Provisioners/localexec.tf
+++ Day4/Provisioners/localexec.tf
@@ -1,3 +1,19 @@
+To get the required commands executed in the Terraform machine, we can use the local provisioners.
+
+Ensure the command is valid on the Terraform running machine.
+
+Copy the below code in the new folder called Localexec
+Use your existing provider credentials
+Run Terraform init
+Terraform apply -auto-approve
+Review the generated Private_ips.txt in your local folder.
+
+
+
+
+
+
+
 resource "aws_instance" "myec2" {
    ami = "ami-082b5a644766e0e6f"
    instance_type = "t2.micro"
