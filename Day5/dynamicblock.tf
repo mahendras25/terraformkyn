a8df7d87d0e3bd59155cef30d54133a9e9863fe3
sampletrngithub01 <135112497+sampletrngithub01@users.noreply.github.com>
2023-11-30 20:22:05 +0530

    Update dynamicblock.tf

--- Day5/dynamicblock.tf
+++ Day5/dynamicblock.tf
@@ -1,1 +1,45 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules for the security group"
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+  ]
+}
+
+resource "aws_security_group" "example" {
+  name        = "example-security-group"
+  description = "Example security group for dynamic blocks"
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  # Additional security group configuration...
+}
 
