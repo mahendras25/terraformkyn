868c053fca1f9d304ecf8cf8f809ac24923655d5
sampletrngithub01 <135112497+sampletrngithub01@users.noreply.github.com>
2023-11-30 20:26:58 +0530

    Update dynamicblock.tf

--- Day5/dynamicblock.tf
+++ Day5/dynamicblock.tf
@@ -1,3 +1,6 @@
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+
+
 provider "aws" {
   region = "us-west-2"
 }
@@ -43,3 +46,33 @@ resource "aws_security_group" "example" {
   # Additional security group configuration...
 }
 
+--------------------
+
+From Documentation
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
